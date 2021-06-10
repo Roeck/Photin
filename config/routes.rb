d22662325b7a0d5b127d41f11641718ed1460c85
Roeck <rons9n@gmail.com>
2021-06-10 14:39:24 -0400

    Account and post associations added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :accounts
+  devise_for :accounts,
+    path: '',
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    controllers: {registrations: 'registrations'}
 
-  get "/dashboard" => "accounts#index"
+    resources :users, only: [:show]
+
+    resources :posts, only: [:index, :show, :create] do   
+      resources :photos, only: [:create]
+
     root to: "public#home"
 end

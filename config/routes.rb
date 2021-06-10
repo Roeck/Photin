063ccd2f6992a497b094b5452eb4f4850c89d3a1
Roeck <rons9n@gmail.com>
2021-06-10 19:18:47 -0400

    Routes bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  root to: "public#home"
+
   devise_for :accounts,
     path: '',
-    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-    controllers: {registrations: 'registrations'}
+    path_names: {sign_in: 'sign_in', sign_out: 'logout', edit: 'profile', sign_up: 'sign_up'}
 
     resources :users, only: [:show]
 
     resources :posts, only: [:index, :show, :create] do   
       resources :photos, only: [:create]
-
-    root to: "public#home"
+    end
 end

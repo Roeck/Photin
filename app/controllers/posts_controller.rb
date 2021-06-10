461a9ee1e019fdc266a68f50bfcc9d3fc9d632df
Roeck <rons9n@gmail.com>
2021-06-10 14:46:42 -0400

    Post controller's create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,22 @@ class PostsController < ApplicationController
         @posts = Post.all.limit(10).includes(:photos)
         @post = Post.new
     end
+
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+        if params[:images]
+            params[:images].each do |img|
+            @post.photos.create(image: params[:images][img])
+            end
+        end
+
+        redirect_to posts_path
+        flash[:notice] = "Success!"
+        else
+        flash[:alert] = "Something went wrong."
+        redirect_to posts_path
+        end
+    end
+
 end

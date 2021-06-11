4088191ab00eb69c766a052be90fdd3887902756
Roeck <rons9n@gmail.com>
2021-06-11 11:42:16 -0400

    Create method fixed/refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,19 +8,13 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = current_user.posts.build(post_params)
-        if @post.save
-        if params[:images]
-            params[:images].each do |img|
-            @post.photos.create(image: params[:images][img])
-            end
-        end
-
+        if
+        Post.create(post_params)
         redirect_to posts_path
         flash[:notice] = "Success!"
         else
-        flash[:alert] = "Something went wrong."
-        redirect_to posts_path
+            flash[:alert] = "Something went wrong"
+            redirect_to posts_path
         end
     end
 
